54db5d3969829ff7ece84f87d095815c9c364628
Matheus Silva Freitas <msformigteen@live.com>
2017-09-01 17:22:55 -0300

    Pronto para Inserir o News

--- resources/views/home/includes/quote.blade.php
+++ resources/views/home/includes/quote.blade.php
@@ -1,20 +1,22 @@
-<div class="ui vertical stripe segment">
+<div class="ui vertical stripe stacked">
     <div class="ui center aligned stackable grid container">
-        <div class="row center aligned">
+        <div class="ui row text container center aligned">
             <h3>"O curso de computação da UFBA é o primeiro do país."</h3>
         </div>
-        <div class="row">
-            <div class="ui center aligned stackable grid container">
-                <div class="row">
-                    <p>
-                        <img src="https://scontent.frec7-1.fna.fbcdn.net/v/t1.0-9/19366470_809547589199020_6045561720722596918_n.jpg?oh=931ff60e5c96d694182085a74afbcf4b&oe=5A263868" class="ui avatar tiny image">
-                    </p>
-                </div>
-                <div class="row">
-                    <p>
-                        <b>Vinicius Drummond</b></br>
-                        <i>Estudante de Ciência da Computação da UFBA</i>
-                    </p>
+        <div class="ui row container">
+            <div class="ui column">
+                <div class="ui center aligned stackable grid container text">
+                    <div class="ui row container">
+                        <p>
+                            <img src="https://scontent.frec7-1.fna.fbcdn.net/v/t1.0-9/19366470_809547589199020_6045561720722596918_n.jpg?oh=931ff60e5c96d694182085a74afbcf4b&oe=5A263868" class="ui avatar tiny image">
+                        </p>
+                    </div>
+                    <div class="ui row container">
+                        <p>
+                            <b>Vinicius Drummond</b></br>
+                            <i>Estudante de Ciência da Computação da UFBA</i>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
